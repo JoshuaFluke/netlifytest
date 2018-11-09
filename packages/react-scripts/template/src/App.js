04bb42259d379e06ec70b5870ef1817bd27b1d73
Joshua Fluke <jfluke717@gmail.com>
2018-11-09 16:31:25 -0700

    confirmed working

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-           LOL CI
+           Confirmed working
            </a>
         </header>
       </div>
